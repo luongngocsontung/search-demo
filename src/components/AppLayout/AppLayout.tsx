bada208f771349cd90edef93647a6ed6588f36a7
Luong Tung <luongngocsontung@gmail.com>
2025-02-20 15:08:01 +0700

    Import svg in vite

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Logo from "@/assets/svgs/logo.svg?react";
 
 const AppLayout = () => {
   return (
     <div>
-      AppLayout
+      {/* Header */}
+      <nav>
+        <div className="flex items-center gap-2 pl-[160px] bg-[#F0F0F0] h-6">
+          <Logo />
+          <p>An Official Website of the Singapore Government</p>
+        </div>
+      </nav>
+
       <Outlet />
     </div>
   );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import AppLayout from "./components/AppLayout";
-import HomePage from "./views/HomePage";
-import NotFoundPage from "./views/NotFoundPage";
+import AppLayout from "@/components/AppLayout";
+import HomePage from "@/views/HomePage";
+import NotFoundPage from "@/views/NotFoundPage";
 
 export const router = createBrowserRouter([
   {
