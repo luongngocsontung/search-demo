c1f27544c7d6029c8a7e9a91fc546a31de5a2e45
Luong Tung <luongngocsontung@gmail.com>
2025-02-26 16:45:50 +0700

    chore: refactor NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,9 @@
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="h-full flex justify-center items-center">
+      <h1>404 - NotFoundPage</h1>
+    </div>
+  );
 };
 
 export default NotFoundPage;
